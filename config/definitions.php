1376c7bfc2eb85ab846f959a77aa98ffc74de791
Script <script@sm.company>
2018-11-06 17:40:33 +0100

    Add vendor files for ORM

---
+++ config/definitions.php
@@ -0,0 +1,29 @@
+<?php 
+
+define('WEBROOT', '/');
+define('DEVELOPMENT_MODE', true);
+
+define('APP_PATH', 'app' . DS);
+define('CONFIG_PATH', 'config' . DS);
+define('BOOTSTRAP_PATH', 'bootstrap' . DS);
+define('CONTROLLERS_PATH', 'app' . DS . 'controllers' . DS);
+define('MODULES_PATH', 'app' . DS . 'modules' . DS);
+define('MODELS_PATH', 'app' . DS . 'models' . DS);
+define('LIBS_PATH', 'app' . DS . 'libs' . DS);
+define('EXCEPTIONS_PATH', 'app' . DS . 'exceptions' . DS);
+define('VIEWS_PATH', 'resources' . DS . 'views' . DS);
+define('LANGS_PATH', 'resources' . DS . 'langs' . DS);
+define('CACHE_PATH', 'storage' . DS . 'cache' . DS);
+define('LOGS_PATH', 'storage' . DS . 'logs' . DS);
+
+function setReporting() {
+	if(DEVELOPMENT_MODE == true) {
+		error_reporting(E_ALL);
+		init_set('display_errors', 'On');
+	} else {
+		error_reporting(E_ALL);
+		init_set('display_errors', 'Off');
+		init_set('log_errors', 'On');
+		init_set('error_log', DOCROOT . LOGS_PATH . 'error.log');
+	}
+}
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+define('DS', DIRECTORY_SEPARATOR);
+define('DOCROOT', dirname(dirname(__FILE__)) . DS);
+
+require DOCROOT . 'config' . DS . 'definitions.php';
+setReporting();
+
+require DOCROOT . 'vendor' . DS . 'autoload.php';
+require DOCROOT . BOOTSTRAP_PATH . 'FrontController.php';
+
+$app = new Bootstrap\FrontController();
+$app->bootstrap();
